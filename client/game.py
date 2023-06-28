287a99d8f1d5b101bba0ab1e33d39d4166fb0d0e
RyanRamos01 <ryanramos@alunos.utfpr.edu.br>
2023-06-28 17:07:03 -0300

    truco rounds finalizado

--- client/game.py
+++ client/game.py
@@ -30,37 +30,84 @@ class TrucoClient:
         self.channel.queue_bind(exchange=self.queues[1], queue=queue_name)              # Vincula a fila à troca do tipo "fanout"
         
         #manda o checkin pro servidor
-        print('publicando checkin:', self.queues[0])
+        # print('publicando checkin:', self.queues[0])
         self.channel.basic_publish(exchange='', routing_key=self.queues[0], body=json.dumps({'cmd': 'checkin', 'nickname': nickname}))
         
-        print('lendo:', queue_name)
+        # print('lendo:', queue_name)
         self.channel.basic_consume(queue_name, self.gameCoordinator) #, no_ack=True) ### se usar auto_ack=true nao precisa do ack dentro do classify
         self.channel.start_consuming()
     #end play
 
     def publish(self, msg):
-        print('DEBUG pub: ', msg)
+        # print('DEBUG pub: ', msg)
         self.channel.basic_publish(exchange='', routing_key=self.queues[0], body=json.dumps(msg))
     #end
 
-    def play(self):
+    def play(self, msg, trucoRes=False):
         #pode jogar uma carta, pedir truco, aumentar, aceitar, correr
-        print(f'SUA VEZ! Digite sua jogada:')
+        print(f'\nSUA VEZ! Digite sua jogada:')
         print(' * Cartas:' + ' '.join([f'({i}){card}' for i, card in enumerate(self.mycards)]) + f' - Vira: [{self.vira}]')
-        print(' * Opções: <carta (index)> / ')
+        
+        #opções
+        options = ''
 
+        if trucoRes == 0: # se nao for resposta
+            options += 'carta <index> / '
+
+            if msg['truco'] == 0: #se nao tiver trucado
+                options += 'truco / '
+            
+            if msg['truco'] < 6 and msg['trucoTeam'] != self.myTeam:
+                options += 'seis / '
+
+
+        if trucoRes == 1: # se for resposta de um truco
+            options += 'aceitar / seis / '
+        
+        if trucoRes == 2: # se for resposta de um seis
+            options += 'aceitar / '
+        
+        #caso tiver trucado, aparecer aviso
+        if msg['truco'] != 0:            
+            print(f" * TRUCADO: {msg['truco']}")
+
+        print(f' * Opções: {options}correr')
+
+        #comandos
         while(True):
-            jogada = input().split(' ')
+            jogada = input('>').split(' ')
 
-            if jogada[0] == 'carta':
+            #correr é sempre uma opção
+            if jogada[0] == 'correr':
+                self.publish({'cmd': 'play', 'type': 'withdraw', 'nickname': self.nickname})
+                break
+            
+            #carta somente quando nao for resposta de truco
+            elif not trucoRes and jogada[0] == 'carta':
                 if len(jogada) < 2:
                     print("-- Erro: Tente: 'carta <index>'")
                     continue
-                index = int(jogada[1]) if int(jogada[1]) >= 0 and int(jogada[1]) < len(self.mycards) else -1
+                index = int(jogada[1]) % len(self.mycards)
                 self.publish({ 'cmd': 'play', 'type': 'card', 'nickname': self.nickname, 'cardindex': index})
                 self.mycards.pop(index)
                 break
             
+            #truco somente quando não está trucado
+            elif not msg['truco'] and jogada[0] == 'truco':
+                self.publish({'cmd': 'play', 'type': 'truco', 'nickname': self.nickname})
+                break
+            
+            #aceitar somente quando é uma resposta de um truco ou seis
+            elif trucoRes and jogada[0] == 'aceitar':
+                self.publish({'cmd': 'play', 'type': 'accept', 'nickname': self.nickname, 'direction': msg['direction']})
+                break
+            
+            # #seis somente quando está trucado, menor que 6(2)e não foi meu time que trucou
+            elif msg['truco'] < 6 and msg['trucoTeam'] != self.myTeam and jogada[0] == 'seis':
+            #     direction = 'tras' if trucoRes == 1 else 'frente'
+                self.publish({'cmd': 'play', 'type': 'seis', 'inResponse': trucoRes, 'nickname': self.nickname})
+                break
+
             else:
                 print('-- Erro: Jogada incorreta')
 
@@ -69,7 +116,7 @@ class TrucoClient:
 
     def gameCoordinator(self, ch, method_frame, header_frame, body):
         msg = json.loads(body)
-        print('DEBUG read: ', msg)
+        # print('DEBUG read: ', msg)
 
         match msg['cmd']:
             case 'checkin':
@@ -95,13 +142,47 @@ class TrucoClient:
                 print('-------------------------------------------------')
                 print(f"MÃO INICIANDO: Ordem de jogadas {msg['order']} / Set {msg['maowinners']}")
                 if msg['nextplayer'] == self.nickname:
-                    self.play()
+                    self.play(msg)
             
             case 'play':
-                print(f" ~ Mesa: {msg['cardsInPlay']} -- {msg['nickname']} jogou [{msg['card']}]")
-                if msg['nextplayer'] == self.nickname:
-                    self.play()
+                if msg['type'] == 'card':
+                    print(f" ~ Mesa: {msg['cardsInPlay']} -- {msg['nickname']} jogou [{msg['card']}]")
+                    if msg['nextplayer'] == self.nickname:
+                        self.play(msg)
+                
+                elif msg['type'] == 'withdraw':
+                    print(f" ~ {msg['nickname']} correu!")
+
+                elif msg['type'] == 'truco':
+                    print(f" ~ {msg['nickname']}: TRUUCOOO!")
+                    if msg['nextplayer'] == self.nickname:
+                        self.play(msg, trucoRes=1)
+
+                elif msg['type'] == 'accept':
+                    print(f" ~ {msg['nickname']}: DEESCEEE!")
+                    if msg['nextplayer'] == self.nickname:
+                        self.play(msg)
+
+                elif msg['type'] == 'seis':
+                    print(f" ~ {msg['nickname']}: SEEIISSS!")
+                    if msg['nextplayer'] == self.nickname:
+                        self.play(msg, trucoRes=2)
             
             case 'result-mao':
-                print(f"MÃO FINALIZADA: Vencedor: Time {msg['winnerTeam']} com a carta {msg['winnerPlayer']} / Set {msg['maowinners']}")
+                print(f"\nMÃO FINALIZADA: Vencedor: Time {msg['winnerTeam']} com a carta {msg['winnerPlayer']} / Set {msg['maowinners']}")
+            
+            case 'result-round':
+                print(f"\nROUND FINALIZADO: Vencedor: Time {msg['winnerTeam']}!!!")
+
+            case 'result-game':
+                print(f"\nJOGO FINALIZADO! VENCEDOR: TIME {msg['winnerTeam']} / PLACAR: {msg['scoreboard'][0]} x {msg['scoreboard'][1]}")
+                if msg['winnerTeam'] == self.myTeam:
+                    print('PARABÉNS!!! VOCÊ GANHOU!')
+                else:
+                    print("Você perdeu :(")
+
+            case 'end-game':
+                print('Finalizando sessão...')
+                self.channel.stop_consuming()
+
 #end game
--- server/game.py
+++ server/game.py
@@ -2,18 +2,21 @@ import pika
 import json
 import random
 import threading
+from statistics import mode
+from time import sleep
 
 class TrucoServer:
     def __init__(self, tablename):
         #table info
         self.tablename = tablename
-        self.baralho = sum([[v+n for n in ['♣','♥','♠','♦']] for v in ['4', '5', '6', '7', 'Q', 'J', 'K', 'A', '2', '3']], [])
+        self.rounds2Win = 12
         # self.playerHash = dict()
 
         #game info
         self.players = {'A': [], 'B': []}
         self.checkin = 0
         self.scoreboard = [0, 0]
+        self.ordemDeEmbaralhar = 0
 
         #round info
         self.vira = None
@@ -26,6 +29,7 @@ class TrucoServer:
         self.maoWinners = 0
         self.nextplayer = None
         self.cardsInPlay = None
+        self.score = 0
 
         #Game Listener
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', '5672'))
@@ -64,6 +68,8 @@ class TrucoServer:
         elif nickname in self.players['B']: self.players['B'].remove(nickname)
         else: return False
         self.checkin -= 1
+        if self.checkin <= 0:
+            return 'delete'
         return True
     #end exit
 
@@ -73,27 +79,33 @@ class TrucoServer:
         self.playOrder = [self.players['A'][0], self.players['B'][0], self.players['A'][1], self.players['B'][1]]
         self.cards = dict()
         self.scoreboard = [0, 0]
-        self.lastWinner = 0
-        self.truco = 0
         self.vira = None
         self.publish({'cmd': 'start-game', 'scoreboard': self.scoreboard, 'timeA': self.players['A'], 'timeB': self.players['B']})
+        self.startRound()
     #end
 
     def startRound(self):
         # vira
-        bar = self.baralho
+        bar = sum([[v+n for n in ['♣','♥','♠','♦']] for v in ['4', '5', '6', '7', 'Q', 'J', 'K', 'A', '2', '3']], [])
         random.shuffle(bar)
         self.vira = bar.pop()
         
         #start round
         self.publish({ 'cmd': 'start-round', 'scoreboard': self.scoreboard, 'vira': self.vira})
         self.maoWinners = []
+        self.score = 1
+        self.lastWinner = self.ordemDeEmbaralhar
+        self.ordemDeEmbaralhar += 1
+        self.truco = 0
+        self.trucoTeam = None
 
         #draw
         for name in (self.players['A'] + self.players['B']):
             cards = [bar.pop() for _ in range(3)]
             self.cards[name] = cards
             self.publish({'cmd': 'draw', 'nickname': name, 'cards': cards})
+        
+        self.startMao()
     #end
 
     def startMao(self):
@@ -101,7 +113,7 @@ class TrucoServer:
         self.playCount = 0 #sempre vai ser 0, pois a ordem já é mudada no playorder --- Contador de cont
         self.nextplayer = self.playOrder[self.playCount]
         self.cardsInPlay = []
-        self.publish({ 'cmd': 'start-mao', 'order': self.playOrder, 'maowinners': self.maoWinners, 'nextplayer': self.nextplayer})
+        self.publish({ 'cmd': 'start-mao', 'order': self.playOrder, 'maowinners': self.maoWinners, 'nextplayer': self.nextplayer, 'truco': False, 'trucoTeam': None})
     #end
 
     # --------------------------------------------------------------
@@ -117,13 +129,60 @@ class TrucoServer:
             self.nextplayer = 'finish'
 
         cip = [c[0] for c in self.cardsInPlay]
-        self.publish({ 'cmd': 'play', 'type': 'card', 'nickname': msg['nickname'], 'card': card, 'nextplayer': self.nextplayer, 'cardsInPlay': cip})
+        self.publish({ 'cmd': 'play', 'type': 'card', 'nickname': msg['nickname'], 'card': card, 'nextplayer': self.nextplayer, 'cardsInPlay': cip, 'truco': self.truco, 'trucoTeam': self.trucoTeam})
     
         if self.nextplayer == 'finish':
-            self.checkMaoWinner() #precisa ser o ultimo, se nao nao manda o print de cima na ordem certa
+            self.checkWinners() #precisa ser o ultimo, caso contrairo, nao manda o print de cima na ordem certa
     #end
 
-    def checkMaoWinner(self):
+    def handleWithdraw(self, nickname):
+        winner = 'A' if nickname in self.players['B'] else 'B'
+        if winner == 'A': self.scoreboard[0] += self.score
+        elif winner == 'B': self.scoreboard[1] += self.score
+        self.publish({'cmd': 'play', 'type': 'withdraw', 'nickname': nickname})
+        self.publish({'cmd': 'result-round', 'winnerTeam': winner})
+        #checar se não houve o vencedor da partida
+        if self.scoreboard[0] >= self.rounds2Win or self.scoreboard[1] >= self.rounds2Win:
+            self.endGame()
+        else:
+            self.startRound()
+    #end withdrw
+
+    def handleTruco(self, nickname):
+        self.truco += 3
+        self.trucoTeam = 'A' if nickname in self.players['A'] else 'B'
+
+        #decidir quem tem que aceitar o truco
+        direction = -1 if self.playCount >= 3 else 1
+        self.nextplayer = self.playOrder[self.playCount + direction]
+        
+        self.publish({'cmd': 'play', 'type': 'truco', 'nickname': nickname, 'nextplayer': self.nextplayer, 'truco': self.truco, 'trucoTeam': self.trucoTeam, 'direction': direction})
+    #end truco
+
+    def handleAccept(self, nickname, direction):
+        self.score = self.truco
+        # print(direction)
+        # direction = direction * -1
+        # print(direction)
+        self.nextplayer = self.playOrder[self.playCount]
+        self.publish({'cmd': 'play', 'type': 'accept', 'nickname': nickname, 'nextplayer': self.nextplayer, 'truco': self.truco, 'trucoTeam': self.trucoTeam})
+    #end accept
+
+    def handleSeis(self, nickname, inResponse):
+        self.truco += 3
+        self.trucoTeam = 'A' if nickname in self.players['A'] else 'B'
+
+        #decidir quem tem que aceitar o seis
+        if inResponse == 1:
+            direction = 0
+        else:
+            direction = -1 if self.playCount >= 3 else 1
+
+        self.nextplayer = self.playOrder[self.playCount + direction]
+        self.publish({'cmd': 'play', 'type': 'seis', 'nickname': nickname, 'nextplayer': self.nextplayer, 'direction': direction, 'truco': self.truco, 'trucoTeam': self.trucoTeam})
+    #end seis
+
+    def checkWinners(self):
         ordemNum = ['4', '5', '6', '7', 'Q', 'J', 'K', 'A', '2', '3']
         ordemNaipe = ['♦','♠','♥', '♣']
         manilha = ordemNum.pop( (ordemNum.index(self.vira[0]) + 1) %  10 )
@@ -143,26 +202,43 @@ class TrucoServer:
                     idx, maiorNum, maiorNaipe = i, num, naipe
 
         
-        #descobrir qual time é o winner:
+        # descobrir que time ganhou a mão:
         winnerPlayer = self.cardsInPlay[idx] # [0] = carta, [1] = nickname
         self.lastWinner = self.playOrder.index(winnerPlayer[1]) #last winner é o index do playorder que vai iniciar o proximo jogo
-
         winnerTeam = 'A' if winnerPlayer[1] in self.players['A'] else 'B'
         self.maoWinners.append(winnerTeam) #define a lista de winners pra saber quem ganha
-       
         self.publish({'cmd': 'result-mao', 'winnerTeam': winnerTeam, 'winnerPlayer': winnerPlayer, 'maowinners': self.maoWinners})
 
-        # checar se ganhou o round
-        if len(self.maoWinners) >= 2:
-            #add points to winner:
-            # if winnerTeam == 'A':
-            #     self.scoreboard[0] += 1
-            # else:
-            #     self.scoreboard[1] += 1
-            pass
+
+        # checar se é a ultima mão e descobrir quem ganhou o round
+        winnerRound = None
+        if self.maoWinners.count('A') == 2: winnerRound = 'A'
+        elif self.maoWinners.count('B') == 2: winnerRound = 'B'
+
+        if winnerRound:
+            if winnerRound == 'A': self.scoreboard[0] += self.score
+            elif winnerRound == 'B': self.scoreboard[1] += self.score
+            self.publish({'cmd': 'result-round', 'winnerTeam': winnerRound})
+
+            #checar se não houve o vencedor da partida
+            if self.scoreboard[0] >= self.rounds2Win or self.scoreboard[1] >= self.rounds2Win:
+                self.endGame()
+            else:
+                self.startRound()
         else:
             self.startMao()
     #end check
+   
+    def endGame(self):
+        winner = 'A' if self.scoreboard[0] >= self.rounds2Win else 'B'
+        self.publish({'cmd': 'result-game', 'winnerTeam': winner, 'scoreboard': self.scoreboard})
+        self.publish({'cmd': 'end-game'})
+        sleep(5)
+        self.channel.queue_delete(queue=self.tablename+'-commands')
+        self.channel.queue_delete(queue=self.tablename+'-clients')
+        self.channel.stop_consuming()
+        # self.connection.
+    #end endgame
 
     # =================================================================
     
@@ -187,13 +263,23 @@ class TrucoServer:
 
                 if self.checkin == 4:
                     self.startGame()
-                    self.startRound()
-                    self.startMao()
             #end checkin
 
             case 'play':
                 if msg['type'] == 'card':
                     self.throwCard(msg)
+                
+                elif msg['type'] == 'withdraw':
+                    self.handleWithdraw(msg['nickname'])
+
+                elif msg['type'] == 'truco':
+                    self.handleTruco(msg['nickname'])
+
+                elif msg['type'] == 'accept':
+                    self.handleAccept(msg['nickname'], msg['direction'])
+
+                elif msg['type'] == 'seis':
+                    self.handleSeis(msg['nickname'], msg['inResponse'])
             #end play
     #end
 
